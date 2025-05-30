4725b732c60cb6983cac2553ab1590d440ddb9a8
AshokNandu-11 <114278147+AshokNandu-11@users.noreply.github.com>
2025-05-30 10:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const sounds = [applause, boo, gasp, tada, victory, wrong, stop];
+const container = document.getElementById("container-buttons");
+sounds.forEach(sound ()=>{
+	const btn = document.createElement("button");
+	btn.innerText = sound;
+
+	btn.addEventListener('click',()=>{
+		const audio = new Audio(`sounds/${sound}`.mp3);
+		audio.play();
+	});
+	container.appendChild(btn);
+});
